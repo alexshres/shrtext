2b3de6ae17af039aaac72de260dbe3470e04d5ab
Alex Shrestha <alexshrestha1996@gmail.com>
2024-03-13 21:03:48 -0700

    added some error handling - didn't really understand

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,12 +16,15 @@ void enableRawMode();
 // state when the program exits
 void disableRawMode();
 
+// Enabling error handling 
+void die(const char* s);
+
 int main() {
   enableRawMode();
 
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     /*
        Printing out each byte that we read()
        We will print each character's numeric ASCII value
@@ -39,7 +43,8 @@ int main() {
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    die("tcgetattr");
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
@@ -85,9 +90,16 @@ void enableRawMode() {
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    die("tcsetattr");
 }
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+void die(const char* s) {
+  perror(s);
+  exit(1);
 }
